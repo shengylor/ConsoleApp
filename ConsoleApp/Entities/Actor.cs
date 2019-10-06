c7c202ad5cd0fef96f1aa9b229942d0a9c7c321d
sheng <shengy@gmail.com>
2019-10-06 18:52:01 -0500

    first commit

---
+++ ConsoleApp/Entities/Actor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp.Entities
+{
+    class Actor
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public bool AwardWinner { get; set; }
+    }
+}
